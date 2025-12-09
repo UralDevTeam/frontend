388a5dc1f16f106005b0b431071b96cfa189e2b7
wswfws <wswfws@ya.ru>
2025-12-09 11:45:54 +0500

    теперь тесты на jwt

--- tests/about.spec.ts
+++ tests/about.spec.ts
@@ -1,18 +1,11 @@
-import {expect, test} from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.describe('Контент страницы "О нас"', () => {
+  test('должна содержать ключевые фразы и термины', async ({ page }) => {
 
-
-  test('должна содержать ключевые фразы и термины', async ({browser}) => {
-    const context = await browser.newContext({
-      extraHTTPHeaders: {
-        'Authorization': 'Basic bWFyaWEua290b3ZhQHVkdi5jb206cGFzc3dvcmQ=',
-      }
-    });
-
-    const page = await context.newPage();
     await page.goto('/about');
 
+    await page.waitForLoadState('networkidle');
 
     const content = (await page.textContent('body'))?.toLowerCase();
 
@@ -29,8 +22,7 @@ test.describe('Контент страницы "О нас"', () => {
     ];
 
     for (const phrase of expectedPhrases) {
-      expect(content).toContain(phrase);
+      expect(content).toContain(phrase.toLowerCase());
     }
   });
-
 });
